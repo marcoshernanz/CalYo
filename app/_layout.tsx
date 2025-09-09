69598d5d5d84297799eaef01ba0c2d3e92aabb56
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-09 17:22:03 +0200

    app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,25 @@
+import AppContextProvider from "@/context/AppContext";
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import {
+  SafeAreaProvider,
+  initialWindowMetrics,
+} from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+        <AppContextProvider>
+          <StatusBar style="light" />
+          <Stack
+            screenOptions={{
+              headerShown: false,
+            }}
+          />
+        </AppContextProvider>
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
+  );
 }
