1a58b98f0c4b4ca69979e103b712baeb8149bb4e
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-09 19:44:41 +0200

    fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,8 +6,62 @@ import {
   SafeAreaProvider,
   initialWindowMetrics,
 } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
+import {
+  useFonts,
+  Inter_100Thin,
+  Inter_100Thin_Italic,
+  Inter_200ExtraLight,
+  Inter_200ExtraLight_Italic,
+  Inter_300Light,
+  Inter_300Light_Italic,
+  Inter_400Regular,
+  Inter_400Regular_Italic,
+  Inter_500Medium,
+  Inter_500Medium_Italic,
+  Inter_600SemiBold,
+  Inter_600SemiBold_Italic,
+  Inter_700Bold,
+  Inter_700Bold_Italic,
+  Inter_800ExtraBold,
+  Inter_800ExtraBold_Italic,
+  Inter_900Black,
+  Inter_900Black_Italic,
+} from "@expo-google-fonts/inter";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Inter_100Thin,
+    Inter_100Thin_Italic,
+    Inter_200ExtraLight,
+    Inter_200ExtraLight_Italic,
+    Inter_300Light,
+    Inter_300Light_Italic,
+    Inter_400Regular,
+    Inter_400Regular_Italic,
+    Inter_500Medium,
+    Inter_500Medium_Italic,
+    Inter_600SemiBold,
+    Inter_600SemiBold_Italic,
+    Inter_700Bold,
+    Inter_700Bold_Italic,
+    Inter_800ExtraBold,
+    Inter_800ExtraBold_Italic,
+    Inter_900Black,
+    Inter_900Black_Italic,
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) return null;
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <SafeAreaProvider initialMetrics={initialWindowMetrics}>
