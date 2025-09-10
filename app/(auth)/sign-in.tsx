6748011ed78444de1c2b5b613723e1e63cc983ac
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-10 20:41:06 +0200

    sign in input

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,17 +1,45 @@
+import Button from "@/components/ui/Button";
+import SafeArea from "@/components/ui/SafeArea";
+import TextInput from "@/components/ui/TextInput";
+import Title from "@/components/ui/Title";
 import { useRouter } from "expo-router";
-import { Button, Text, View } from "react-native";
+import { ArrowLeftIcon } from "lucide-react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function SignIn() {
   const router = useRouter();
 
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Sign In Screen</Text>
-      {/* Placeholder; no real auth logic */}
+    <SafeArea>
       <Button
-        title="Sign In"
-        onPress={() => router.replace({ pathname: "/home" as any })}
-      />
-    </View>
+        variant="secondary"
+        style={styles.closeButton}
+        onPress={() => router.back()}
+      >
+        <ArrowLeftIcon />
+      </Button>
+      <View style={styles.container}>
+        <Title>Iniciar Sesión</Title>
+        <TextInput
+          label="Email"
+          placeholder="example@example.com"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+      </View>
+      <Button size="lg">Comenzar</Button>
+    </SafeArea>
   );
 }
+
+const styles = StyleSheet.create({
+  closeButton: {
+    borderRadius: 9999,
+    aspectRatio: 1,
+  },
+  container: {
+    flex: 1,
+    paddingVertical: 20,
+    gap: 32,
+  },
+});
