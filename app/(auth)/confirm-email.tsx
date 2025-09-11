2526c7c7a066b350e495c35c7fdaf17e5862527e
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-11 16:41:05 +0200

    confirm email

---
+++ app/(auth)/confirm-email.tsx
@@ -0,0 +1,60 @@
+import Button from "@/components/ui/Button";
+import Description from "@/components/ui/Description";
+import Header from "@/components/ui/Header";
+import SafeArea from "@/components/ui/SafeArea";
+import Text from "@/components/ui/Text";
+import Title from "@/components/ui/Title";
+import { useRouter } from "expo-router";
+import { ArrowLeftIcon } from "lucide-react-native";
+import { StyleSheet, View } from "react-native";
+import { KeyboardAvoidingView } from "react-native-keyboard-controller";
+
+export default function ConfirmEmailScreen() {
+  const router = useRouter();
+
+  const handleSubmit = () => {};
+
+  return (
+    <SafeArea>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior="padding"
+        keyboardVerticalOffset={10}
+      >
+        <Button
+          variant="secondary"
+          style={styles.closeButton}
+          onPress={() => router.back()}
+        >
+          <ArrowLeftIcon />
+        </Button>
+        <View style={styles.container}>
+          <Header>
+            <Title>Confirma tu Email</Title>
+            <Description>
+              Introduce el código que te acabamos de enviar a{" "}
+              <Text size="16" style={{ fontWeight: 600 }}>
+                em••••@gmail.com
+              </Text>
+            </Description>
+          </Header>
+        </View>
+        <Button size="lg" onPress={handleSubmit}>
+          Continuar
+        </Button>
+      </KeyboardAvoidingView>
+    </SafeArea>
+  );
+}
+
+const styles = StyleSheet.create({
+  closeButton: {
+    borderRadius: 9999,
+    aspectRatio: 1,
+  },
+  container: {
+    flex: 1,
+    paddingVertical: 20,
+    gap: 32,
+  },
+});
