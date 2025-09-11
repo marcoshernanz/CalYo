24f3bb96aaea747628c57745439b4683749e81f3
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-11 18:17:43 +0200

    opt input

---
+++ components/ui/OTPInput.tsx
@@ -0,0 +1,127 @@
+import {
+  Pressable,
+  StyleSheet,
+  TextInput,
+  TextInputProps,
+  View,
+} from "react-native";
+import { useRef, useState } from "react";
+import Text from "./Text";
+import { useSharedValue, withTiming } from "react-native-reanimated";
+import getColor from "@/lib/utils/getColor";
+
+export type OTPInputHandle = {
+  focus: () => void;
+  flashError: () => void;
+};
+
+interface Props extends TextInputProps {
+  ref?: React.Ref<OTPInputHandle>;
+  length?: number;
+  onFilled?: (code: string) => void;
+}
+
+export default function OTPInput({
+  length = 4,
+  onFilled,
+  ref,
+  ...props
+}: Props) {
+  const [text, setText] = useState("");
+  const [isFocused, setIsFocused] = useState(props.autoFocus);
+
+  const inputRef = useRef<TextInput>(null);
+
+  const focusedInputIndex = text.length;
+
+  const isFocusedShared = useSharedValue(0);
+
+  const handleTextChange = (value: string) => {
+    if (/[^\d]/.test(value)) return;
+    setText(value);
+    props.onChangeText?.(value);
+    if (value.length === length) {
+      onFilled?.(value);
+      inputRef.current?.blur();
+    }
+  };
+
+  const handleFocus = (e: any) => {
+    setIsFocused(true);
+    isFocusedShared.value = withTiming(1, { duration: 200 });
+    props.onFocus?.(e);
+  };
+
+  const handleBlur = (e: any) => {
+    setIsFocused(false);
+    isFocusedShared.value = withTiming(0, { duration: 200 });
+    props.onBlur?.(e);
+  };
+
+  return (
+    <View style={styles.container}>
+      {Array(length)
+        .fill(0)
+        .map((_, index) => {
+          const isFocusedInput =
+            Boolean(isFocused) &&
+            (index === focusedInputIndex ||
+              (index === length - 1 && text.length === length));
+          const char = text[index] || "";
+
+          return (
+            <Pressable
+              key={index}
+              onPress={() => inputRef.current?.focus()}
+              style={[styles.box, isFocusedInput && styles.focusedBox]}
+            >
+              <Text size="28" style={styles.text}>
+                {char}
+              </Text>
+            </Pressable>
+          );
+        })}
+      <TextInput
+        value={text}
+        onChangeText={handleTextChange}
+        maxLength={length}
+        inputMode="numeric"
+        textContentType="oneTimeCode"
+        ref={inputRef}
+        autoComplete={"one-time-code"}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        caretHidden
+        {...props}
+        style={[styles.input, props.style]}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    gap: 16,
+    height: 100,
+  },
+  box: {
+    flex: 1,
+    borderColor: getColor("mutedForeground"),
+    borderWidth: 1,
+    borderRadius: 12,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  focusedBox: {
+    borderColor: getColor("foreground"),
+    borderWidth: 2,
+  },
+  text: {
+    fontWeight: 700,
+  },
+  input: {
+    ...StyleSheet.absoluteFillObject,
+    opacity: 0,
+  },
+});
