5a4fce6d02f3f36209801b8d4fa3251a8fce2766
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-11 16:41:10 +0200

    description component

--- components/ui/Description.tsx
+++ components/ui/Description.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { StyleSheet } from "react-native";
 import Text, { TextProps } from "./Text";
+import getColor from "@/lib/utils/getColor";
 
 export default function Description(rest: TextProps) {
-  return <Text {...rest} size="18" style={[styles.description, rest.style]} />;
+  return <Text {...rest} size="16" style={[styles.description, rest.style]} />;
 }
 
 const styles = StyleSheet.create({
-  description: {},
+  description: {
+    color: getColor("mutedForeground"),
+  },
 });
