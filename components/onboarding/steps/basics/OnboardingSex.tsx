022e67d7c2ea1a4741cde66a7ab0ee78c0061684
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-13 23:00:53 +0200

    onboarding sex question

--- components/onboarding/steps/basics/OnboardingSex.tsx
+++ components/onboarding/steps/basics/OnboardingSex.tsx
@@ -1,9 +1,22 @@
+import Select from "@/components/ui/Select";
 import Title from "@/components/ui/Title";
+import { MarsIcon, VenusIcon } from "lucide-react-native";
+import { useState } from "react";
 
 export default function OnboardingSex() {
+  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+
   return (
     <>
       <Title size="24">¿Cuál es tu sexo?</Title>
+      <Select
+        options={[
+          { name: "Hombre", label: "Hombre", Icon: MarsIcon },
+          { name: "Mujer", label: "Mujer", Icon: VenusIcon },
+        ]}
+        selectedOptions={selectedOption ? [selectedOption] : []}
+        onSelectOption={setSelectedOption}
+      />
     </>
   );
 }
