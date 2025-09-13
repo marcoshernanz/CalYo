c2b61ba1e559a31a44269eb5aff782dc44359542
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-13 16:15:17 +0200

    onboarding basics section

--- components/onboarding/steps/basics/OnboardingBasicsSection.tsx
+++ components/onboarding/steps/basics/OnboardingBasicsSection.tsx
@@ -1,3 +1,21 @@
+import Description from "@/components/ui/Description";
+import Header from "@/components/ui/Header";
+import Title from "@/components/ui/Title";
+import { StyleSheet } from "react-native";
+
 export default function OnboardingBasicsSection() {
-  return null;
+  return (
+    <>
+      <Header style={styles.header}>
+        <Title>¡Empecemos!</Title>
+        <Description>Tu programa personalizado te espera</Description>
+      </Header>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    alignItems: "center",
+  },
+});
