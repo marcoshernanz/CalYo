5935ff2f56a9d2e712e590ee0824b5486cf39977
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-15 14:34:56 +0200

    link onboarding sex select to onboarding context

--- components/onboarding/steps/basics/OnboardingSex.tsx
+++ components/onboarding/steps/basics/OnboardingSex.tsx
@@ -1,20 +1,29 @@
 import Select from "@/components/ui/Select";
 import Title from "@/components/ui/Title";
+import { useOnboardingContext } from "@/context/OnboardingContext";
 import { MarsIcon, VenusIcon } from "lucide-react-native";
-import { useState } from "react";
+
+const options = [
+  { name: "male", label: "Hombre", Icon: MarsIcon },
+  { name: "female", label: "Mujer", Icon: VenusIcon },
+];
 
 export default function OnboardingSex() {
-  const [selectedOption, setSelectedOption] = useState<string | null>(null);
+  const { data, setData } = useOnboardingContext();
+
+  const selectedOptions = data.sex ? [data.sex] : [];
+  const setSelectedOption = (name: string) => {
+    if (name === "male" || name === "female") {
+      setData((prev) => ({ ...prev, sex: name }));
+    }
+  };
 
   return (
     <>
       <Title size="24">¿Cuál es tu sexo?</Title>
       <Select
-        options={[
-          { name: "Hombre", label: "Hombre", Icon: MarsIcon },
-          { name: "Mujer", label: "Mujer", Icon: VenusIcon },
-        ]}
-        selectedOptions={selectedOption ? [selectedOption] : []}
+        options={options}
+        selectedOptions={selectedOptions}
         onSelectOption={setSelectedOption}
       />
     </>
