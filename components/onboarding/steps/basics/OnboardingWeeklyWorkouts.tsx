a867e9b4ad2dd74dfc6a911ae3bcc755c3cffcea
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-15 15:55:55 +0200

    onboarding weekly workouts

--- components/onboarding/steps/basics/OnboardingWeeklyWorkouts.tsx
+++ components/onboarding/steps/basics/OnboardingWeeklyWorkouts.tsx
@@ -1,9 +1,46 @@
+import Select, { SelectOption } from "@/components/ui/Select";
 import Title from "@/components/ui/Title";
+import { useOnboardingContext } from "@/context/OnboardingContext";
+
+const options: SelectOption[] = [
+  {
+    name: "0-2",
+    label: "0-2",
+    description: "Entreno de vez en cuando",
+    Icon: <></>,
+  },
+  {
+    name: "3-5",
+    label: "3-5",
+    description: "Entreno varias veces por semana",
+    Icon: <></>,
+  },
+  {
+    name: "6+",
+    label: "6+",
+    description: "Entreno casi a diario",
+    Icon: <></>,
+  },
+];
 
 export default function OnboardingWeeklyWorkouts() {
+  const { data, setData } = useOnboardingContext();
+
+  const selectedOptions = data.weeklyWorkouts ? [data.weeklyWorkouts] : [];
+  const setSelectedOption = (name: string) => {
+    if (name === "0-2" || name === "3-5" || name === "6+") {
+      setData((prev) => ({ ...prev, weeklyWorkouts: name }));
+    }
+  };
+
   return (
     <>
       <Title size="24">¿Cuantos entrenamientos haces a la semana?</Title>
+      <Select
+        options={options}
+        selectedOptions={selectedOptions}
+        onSelectOption={setSelectedOption}
+      />
     </>
   );
 }
