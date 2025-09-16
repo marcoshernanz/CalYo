9755afbf6bafdecedce3c81702b2501607da66b1
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-16 15:45:29 +0200

    onboarding weekly workouts icons

--- components/onboarding/steps/basics/OnboardingWeeklyWorkouts.tsx
+++ components/onboarding/steps/basics/OnboardingWeeklyWorkouts.tsx
@@ -1,25 +1,28 @@
+import Dot3Icon from "@/components/icons/Dot3Icon";
+import Dot1Icon from "@/components/icons/Dot1Icon";
 import Select, { SelectOption } from "@/components/ui/Select";
 import Title from "@/components/ui/Title";
 import { useOnboardingContext } from "@/context/OnboardingContext";
+import Dot6Icon from "@/components/icons/Dot6Icon";
 
 const options: SelectOption[] = [
   {
     name: "0-2",
     label: "0-2",
     description: "Entreno de vez en cuando",
-    Icon: <></>,
+    Icon: Dot1Icon,
   },
   {
     name: "3-5",
     label: "3-5",
     description: "Entreno varias veces por semana",
-    Icon: <></>,
+    Icon: Dot3Icon,
   },
   {
     name: "6+",
     label: "6+",
     description: "Entreno casi a diario",
-    Icon: <></>,
+    Icon: Dot6Icon,
   },
 ];
 
