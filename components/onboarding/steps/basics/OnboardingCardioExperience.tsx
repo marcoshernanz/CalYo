708633cbd36147f40abcd93c79b8cad4839dd8cd
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-16 20:10:27 +0200

    onboarding cardio experience

--- components/onboarding/steps/basics/OnboardingCardioExperience.tsx
+++ components/onboarding/steps/basics/OnboardingCardioExperience.tsx
@@ -1,9 +1,63 @@
+import Select, { SelectOption } from "@/components/ui/Select";
 import Title from "@/components/ui/Title";
+import { useOnboardingContext } from "@/context/OnboardingContext";
+import {
+  SignalIcon,
+  SignalLowIcon,
+  SignalMediumIcon,
+  SignalZeroIcon,
+} from "lucide-react-native";
+
+const options: SelectOption[] = [
+  {
+    name: "none",
+    label: "Ninguna",
+    description: "No hago cardio",
+    Icon: SignalZeroIcon,
+  },
+  {
+    name: "beginner",
+    label: "Principiante",
+    description: "Llevo menos de 1 año haciendo cardio",
+    Icon: SignalLowIcon,
+  },
+  {
+    name: "intermediate",
+    label: "Intermedio",
+    description: "Llevo entre 1 y 4 años haciendo cardio",
+    Icon: SignalMediumIcon,
+  },
+  {
+    name: "advanced",
+    label: "Avanzado",
+    description: "Llevo más de 4 años haciendo cardio",
+    Icon: SignalIcon,
+  },
+];
 
 export default function OnboardingCardioExperience() {
+  const { data, setData } = useOnboardingContext();
+
+  const selectedOptions = data.cardioExperience ? [data.cardioExperience] : [];
+  const setSelectedOption = (name: string) => {
+    if (
+      name === "none" ||
+      name === "beginner" ||
+      name === "intermediate" ||
+      name === "advanced"
+    ) {
+      setData((prev) => ({ ...prev, cardioExperience: name }));
+    }
+  };
+
   return (
     <>
       <Title size="24">¿Cual es tu experiencia con el cardio?</Title>
+      <Select
+        options={options}
+        selectedOptions={selectedOptions}
+        onSelectOption={setSelectedOption}
+      />
     </>
   );
 }
