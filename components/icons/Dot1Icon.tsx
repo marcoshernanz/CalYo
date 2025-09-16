afa236c483d7fc29dd057685c7442c0c27132e21
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-16 15:45:07 +0200

    dot 1 icon

---
+++ components/icons/Dot1Icon.tsx
@@ -0,0 +1,38 @@
+import getColor from "@/lib/utils/getColor";
+import { StyleSheet, View } from "react-native";
+
+interface Props {
+  size?: number;
+  color?: string;
+}
+
+export default function Dot1Icon({
+  size = 24,
+  color = getColor("foreground"),
+}: Props) {
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          width: size,
+          height: size,
+        },
+      ]}
+    >
+      <View style={[styles.dot, { backgroundColor: color }]} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  dot: {
+    width: "60%",
+    height: "60%",
+    borderRadius: 999,
+  },
+});
