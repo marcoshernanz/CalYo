9e512c6fd1262af663c8d67e9fd3cacf3f509c10
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-16 15:49:16 +0200

    onboarding activity level

--- components/onboarding/steps/basics/OnboardingActivityLevel.tsx
+++ components/onboarding/steps/basics/OnboardingActivityLevel.tsx
@@ -1,9 +1,31 @@
+import Select, { SelectOption } from "@/components/ui/Select";
 import Title from "@/components/ui/Title";
+import { useOnboardingContext } from "@/context/OnboardingContext";
+
+const options: SelectOption[] = [
+  { name: "low", label: "Sedentario", description: "", Icon: <></> },
+  { name: "medium", label: "Algo Activo", description: "", Icon: <></> },
+  { name: "high", label: "Muy Activo", description: "", Icon: <></> },
+];
 
 export default function OnboardingActivityLevel() {
+  const { data, setData } = useOnboardingContext();
+
+  const selectedOptions = data.activityLevel ? [data.activityLevel] : [];
+  const setSelectedOption = (name: string) => {
+    if (name === "low" || name === "medium" || name === "high") {
+      setData((prev) => ({ ...prev, activityLevel: name }));
+    }
+  };
+
   return (
     <>
       <Title size="24">¿Cuál es tu nivel de actividad?</Title>
+      <Select
+        options={options}
+        selectedOptions={selectedOptions}
+        onSelectOption={setSelectedOption}
+      />
     </>
   );
 }
