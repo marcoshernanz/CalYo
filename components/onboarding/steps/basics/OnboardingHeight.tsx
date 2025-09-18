79b141b459530ba3763b88c63d4b2b32681497d7
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-18 19:47:12 +0200

    onboarding height measurement system picker

--- components/onboarding/steps/basics/OnboardingHeight.tsx
+++ components/onboarding/steps/basics/OnboardingHeight.tsx
@@ -1,15 +1,36 @@
+import SegmentedControl from "@/components/ui/SegmentedControl";
 import Title from "@/components/ui/Title";
 import WheelPicker from "@/components/ui/WheelPicker";
 import { useOnboardingContext } from "@/context/OnboardingContext";
 import { Dimensions, StyleSheet, View } from "react-native";
 
 export default function OnboardingHeight() {
-  const { setData } = useOnboardingContext();
+  const { data, setData } = useOnboardingContext();
+
+  const handleMeasurementSystemChange = (system: string) => {
+    if (system !== "Centímetros" && system !== "Pies y Pulgadas") return;
+    setData((prev) => ({
+      ...prev,
+      measurementSystem: system === "Centímetros" ? "metric" : "imperial",
+    }));
+  };
 
   return (
     <>
       <Title size="24">¿Cuanto mides?</Title>
       <View style={style.pickerContainer}>
+        <View style={style.segmentControlContainer}>
+          <SegmentedControl
+            options={["Pies y Pulgadas", "Centímetros"]}
+            selectedOption={
+              data.measurementSystem === "metric"
+                ? "Centímetros"
+                : "Pies y Pulgadas"
+            }
+            onChange={handleMeasurementSystemChange}
+          />
+        </View>
+
         <WheelPicker
           data={Array.from({ length: 91 }, (_, i) => `${120 + i} cm`)}
           onValueChange={(value) =>
@@ -21,7 +42,7 @@ export default function OnboardingHeight() {
               })(),
             }))
           }
-          defaultValue="170 cm" // TODO
+          initialValue="170 cm"
         />
       </View>
     </>
@@ -29,6 +50,11 @@ export default function OnboardingHeight() {
 }
 
 const style = StyleSheet.create({
+  segmentControlContainer: {
+    position: "absolute",
+    left: "50%",
+    transform: [{ translateX: "-50%" }],
+  },
   pickerContainer: {
     width: Dimensions.get("window").width,
     marginLeft: -16,
