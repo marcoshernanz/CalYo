6191338f9b0f7358c9ab5b0135f4b792313c9dc0
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-22 19:58:51 +0200

    onboarding weight change rate highlight

--- components/onboarding/steps/goal/OnboardingWeightChangeRate.tsx
+++ components/onboarding/steps/goal/OnboardingWeightChangeRate.tsx
@@ -3,6 +3,10 @@ import Title from "@/components/ui/Title";
 import { View } from "react-native";
 import { useSharedValue } from "react-native-reanimated";
 
+const minValue = 0.1;
+const maxValue = 2;
+const recommendedRange = [0.2, 0.8] as [number, number];
+
 export default function OnboardingWeightChangeRate() {
   const changeRate = useSharedValue(50);
 
@@ -10,7 +14,12 @@ export default function OnboardingWeightChangeRate() {
     <>
       <Title size="24">¿Cómo de rápido quieres alcanzar tu objetivo?</Title>
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Slider minValue={0} maxValue={100} value={changeRate} />
+        <Slider
+          minValue={minValue}
+          maxValue={maxValue}
+          value={changeRate}
+          highlightedRange={recommendedRange}
+        />
       </View>
     </>
   );
