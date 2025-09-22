0e90c9581bd24fd83b61a9cd06572d704a8ed2cb
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-09-22 20:45:25 +0200

    primary light color

--- lib/utils/getColor.ts
+++ lib/utils/getColor.ts
@@ -1,5 +1,6 @@
 const colors = {
   primary: [59, 130, 246], // #3B82F6
+  primaryLight: [191, 219, 254], // #bfdbfe
   background: [249, 250, 251], // #f9fafb
   foreground: [3, 7, 18], // #030712
   mutedForeground: [107, 114, 128], // #6b7280
