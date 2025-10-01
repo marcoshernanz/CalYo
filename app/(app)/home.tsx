2f2598a4ed624cbc5afe769fe62e002f4afe0013
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-01 20:21:11 +0200

    home

--- app/(app)/home.tsx
+++ app/(app)/home.tsx
@@ -1,9 +1,38 @@
-import { Text, View } from "react-native";
+import Button from "@/components/ui/Button";
+import SafeArea from "@/components/ui/SafeArea";
+import Text from "@/components/ui/Text";
+import { useAuthContext } from "@/context/AuthContext";
+import { useRouter } from "expo-router";
+import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
+  const { signOut } = useAuthContext();
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    await signOut();
+    router.replace("/auth");
+  };
+
   return (
-    <View>
-      <Text>Home Screen</Text>
-    </View>
+    <SafeArea>
+      <View style={styles.container}>
+        <Text size="20" style={{ fontWeight: 600 }}>
+          Home Screen
+        </Text>
+        <Button size="lg" onPress={handleSignOut}>
+          Cerrar sesión
+        </Button>
+      </View>
+    </SafeArea>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 24,
+  },
+});
