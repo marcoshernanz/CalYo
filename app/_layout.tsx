6aaeef9184da66eeb4035d5e552eba156d722bdb
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-01 20:10:59 +0200

    protected routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,29 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import RootLayoutProvider from "@/components/RootLayoutProvider";
+import { useAuthContext } from "@/context/AuthContext";
 
 export default function RootLayout() {
   return (
     <RootLayoutProvider>
       <StatusBar style="light" />
-      <Stack
-        screenOptions={{
-          headerShown: false,
-        }}
-      />
+      <RootNavigator />
     </RootLayoutProvider>
+  );
+}
+
+function RootNavigator() {
+  const { isAuthenticated } = useAuthContext();
+
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Protected guard={isAuthenticated}>
+        <Stack.Screen name="(app)" />
+      </Stack.Protected>
+
+      <Stack.Protected guard={!isAuthenticated}>
+        <Stack.Screen name="sign-in" />
+      </Stack.Protected>
+    </Stack>
   );
 }
