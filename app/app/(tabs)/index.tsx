8a2174123cd3da1ba0296759c99f2c2cbf8350fd
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-06 20:10:22 +0200

    refactor home header

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,34 +1,25 @@
 import CalyoLogo from "@/assets/svg/calyo-logo.svg";
+import HomeDaySelector from "@/components/home/HomeDaySelector";
+import HomeHeader from "@/components/home/HomeHeader";
 import SafeArea from "@/components/ui/SafeArea";
 import Text from "@/components/ui/Text";
 import getColor from "@/lib/utils/getColor";
 import { LinearGradient } from "expo-linear-gradient";
 import { FlameIcon } from "lucide-react-native";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, useWindowDimensions, View } from "react-native";
 
 export default function HomeScreen() {
+  const dimensions = useWindowDimensions();
+
   return (
     <SafeArea>
       <LinearGradient
-        colors={[getColor("primaryLight"), getColor("background")]}
-        style={styles.gradient}
+        colors={[getColor("primaryLight", 0.5), getColor("background")]}
+        style={[styles.gradient, { height: dimensions.height / 2 }]}
       />
-      <View style={styles.header}>
-        <View style={styles.logoContainer}>
-          <CalyoLogo width={28} height={28} color={getColor("foreground")} />
-          <Text size="28" style={styles.logoText}>
-            CalYo
-          </Text>
-        </View>
-        <View style={styles.streakContainer}>
-          <FlameIcon
-            size={22}
-            color={getColor("orange")}
-            fill={getColor("orange")}
-          />
-          <Text style={styles.streakText}>12</Text>
-        </View>
-      </View>
+
+      <HomeHeader />
+      <HomeDaySelector />
     </SafeArea>
   );
 }
@@ -36,31 +27,5 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   gradient: {
     ...StyleSheet.absoluteFillObject,
-  },
-  header: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  logoContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: 8,
-  },
-  logoText: {
-    fontWeight: 600,
-  },
-  streakContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: getColor("background"),
-    padding: 8,
-    gap: 4,
-    borderRadius: 999,
-  },
-  streakText: {
-    fontWeight: 600,
   },
 });
---
+++ components/home/HomeHeader.tsx
@@ -0,0 +1,55 @@
+import { StyleSheet, View } from "react-native";
+import CalyoLogo from "@/assets/svg/calyo-logo.svg";
+import getColor from "@/lib/utils/getColor";
+import Text from "../ui/Text";
+import { FlameIcon } from "lucide-react-native";
+
+export default function HomeHeader() {
+  return (
+    <View style={styles.header}>
+      <View style={styles.logoContainer}>
+        <CalyoLogo width={28} height={28} color={getColor("foreground")} />
+        <Text size="28" style={styles.logoText}>
+          CalYo
+        </Text>
+      </View>
+      <View style={styles.streakContainer}>
+        <FlameIcon
+          size={22}
+          color={getColor("orange")}
+          fill={getColor("orange")}
+        />
+        <Text style={styles.streakText}>12</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  logoContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+  },
+  logoText: {
+    fontWeight: 600,
+  },
+  streakContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: getColor("background"),
+    padding: 8,
+    gap: 4,
+    borderRadius: 999,
+  },
+  streakText: {
+    fontWeight: 600,
+  },
+});
