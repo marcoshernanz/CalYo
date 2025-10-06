32af0f7c7ab83ec6b57fca37c05a6071445e89cd
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-06 15:41:46 +0200

    index header

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,38 +1,58 @@
-import Button from "@/components/ui/Button";
+import CalyoLogo from "@/assets/svg/calyo-logo.svg";
 import SafeArea from "@/components/ui/SafeArea";
 import Text from "@/components/ui/Text";
-import { useAuthContext } from "@/context/AuthContext";
-import { useRouter } from "expo-router";
+import getColor from "@/lib/utils/getColor";
+import { FlameIcon } from "lucide-react-native";
 import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
-  const { signOut } = useAuthContext();
-  const router = useRouter();
-
-  const handleSignOut = async () => {
-    await signOut();
-    router.replace("/auth");
-  };
-
   return (
     <SafeArea>
-      <View style={styles.container}>
-        <Text size="20" style={{ fontWeight: 600 }}>
-          Home Screen
-        </Text>
-        <Button size="lg" onPress={handleSignOut}>
-          Cerrar sesión
-        </Button>
+      <View style={styles.header}>
+        <View style={styles.logoContainer}>
+          <CalyoLogo width={28} height={28} color={getColor("foreground")} />
+          <Text size="28" style={styles.logoText}>
+            CalYo
+          </Text>
+        </View>
+        <View style={styles.streakContainer}>
+          <FlameIcon
+            size={22}
+            color={getColor("orange")}
+            fill={getColor("orange")}
+          />
+          <Text style={styles.streakText}>12</Text>
+        </View>
       </View>
     </SafeArea>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    gap: 24,
+  },
+  logoContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 8,
+  },
+  logoText: {
+    fontWeight: 600,
+  },
+  streakContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: getColor("background"),
+    padding: 8,
+    gap: 4,
+    borderRadius: 999,
+  },
+  streakText: {
+    fontWeight: 600,
   },
 });
