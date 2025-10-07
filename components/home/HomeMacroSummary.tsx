6dc1a95469ca5428d90899c460a72a4ebf9eda84
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-07 15:57:16 +0200

    calyo macro card

--- components/home/HomeMacroSummary.tsx
+++ components/home/HomeMacroSummary.tsx
@@ -4,6 +4,27 @@ import Text from "../ui/Text";
 import getColor from "@/lib/utils/getColor";
 import CircularProgress from "../ui/CircularProgress";
 
+const macros = [
+  {
+    name: "Hidratos",
+    value: 250,
+    target: 300,
+    Icon: LucideWheat,
+  },
+  {
+    name: "Proteína",
+    value: 150,
+    target: 200,
+    Icon: LucideWheat,
+  },
+  {
+    name: "Grasas",
+    value: 70,
+    target: 100,
+    Icon: LucideWheat,
+  },
+];
+
 type Macro = {
   name: string;
   value: number;
@@ -17,35 +38,40 @@ interface MacroCardProps {
 
 function MacroCard({ macro }: MacroCardProps) {
   return (
-    <View>
-      <macro.Icon />
-      <Text>{macro.name}</Text>
-      <Text>{macro.value}</Text>
-      <Text>{macro.target}</Text>
+    <View style={styles.macroCard}>
+      <Text size="16" weight="500">
+        {macro.name}
+      </Text>
+      <View style={styles.caloriesValueContainer}>
+        <Text size="20" weight="600">
+          {macro.value}
+        </Text>
+        <Text
+          size="12"
+          color={getColor("mutedForeground")}
+          style={styles.targetText}
+        >
+          {" "}
+          / {macro.target}
+        </Text>
+      </View>
+      {/* <View>
+        <macro.Icon />
+      </View> */}
+
+      <View style={styles.progressContainer}>
+        <CircularProgress
+          progress={1532 / 2000}
+          color={getColor("foreground")}
+          strokeWidth={6}
+        />
+        <View style={styles.caloriesIconContainer}>
+          <FlameIcon size={20} />
+        </View>
+      </View>
     </View>
   );
 }
-
-const macros = [
-  {
-    name: "Carbohidratos",
-    value: 250,
-    target: 300,
-    Icon: LucideWheat,
-  },
-  {
-    name: "Proteínas",
-    value: 150,
-    target: 200,
-    Icon: LucideWheat,
-  },
-  {
-    name: "Grasas",
-    value: 70,
-    target: 100,
-    Icon: LucideWheat,
-  },
-];
 
 export default function HomeMacroSummary() {
   return (
@@ -90,7 +116,9 @@ export default function HomeMacroSummary() {
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    gap: 12,
+  },
   caloriesContainer: {
     backgroundColor: getColor("background"),
     padding: 20,
@@ -98,6 +126,8 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: getColor("secondary"),
   },
   caloriesTextContainer: {},
   caloriesValueContainer: {
@@ -124,5 +154,15 @@ const styles = StyleSheet.create({
   },
   cardsContainer: {
     flexDirection: "row",
+    gap: 12,
+  },
+
+  macroCard: {
+    flex: 1,
+    backgroundColor: getColor("background"),
+    padding: 16,
+    borderRadius: 16,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: getColor("secondary"),
   },
 });
