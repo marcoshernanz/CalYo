197299a8aea25b3320cae69c51f00526505f2b7a
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-07 15:09:46 +0200

    home macro summary sketch

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,15 +1,13 @@
 import HomeDaySelector from "@/components/home/HomeDaySelector";
 import HomeHeader from "@/components/home/HomeHeader";
-import Button from "@/components/ui/Button";
+import HomeMacroSummary from "@/components/home/HomeMacroSummary";
 import SafeArea from "@/components/ui/SafeArea";
-import { useAuthContext } from "@/context/AuthContext";
 import getColor from "@/lib/utils/getColor";
 import { LinearGradient } from "expo-linear-gradient";
 import { StyleSheet, useWindowDimensions } from "react-native";
 
 export default function HomeScreen() {
   const dimensions = useWindowDimensions();
-  const { signOut } = useAuthContext();
 
   return (
     <SafeArea>
@@ -20,7 +18,7 @@ export default function HomeScreen() {
 
       <HomeHeader />
       <HomeDaySelector />
-      {/* <Button onPress={signOut}>Sign Out</Button> */}
+      <HomeMacroSummary />
     </SafeArea>
   );
 }
