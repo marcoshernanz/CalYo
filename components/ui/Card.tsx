39a5c8dde76f641e2fcc0408b01a5d5ae4ea9d16
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-08 21:01:12 +0200

    card ui component

---
+++ components/ui/Card.tsx
@@ -0,0 +1,26 @@
+import getColor from "@/lib/ui/getColor";
+import getShadow from "@/lib/ui/getShadow";
+import { StyleSheet, View, ViewProps } from "react-native";
+
+interface Props extends ViewProps {
+  children: React.ReactNode;
+}
+
+export default function Card({ children, style, ...props }: Props) {
+  return (
+    <View style={[styles.card, style]} {...props}>
+      {children}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: getColor("white"),
+    padding: 20,
+    borderRadius: 16,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: getColor("secondary"),
+    ...getShadow("sm"),
+  },
+});
