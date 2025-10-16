eff774fe6590085123e8b3135d6b1dc7f062f0e5
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-16 19:04:25 +0200

    remove comment

--- convex/meals/analyzeMealPicture.ts
+++ convex/meals/analyzeMealPicture.ts
@@ -36,18 +36,6 @@ const analyzeMealPicture = action({
         },
       },
     });
-
-    // const uploadUrl = await ctx.runMutation(
-    //   api.storage.generateUploadUrl.default
-    // );
-    // const response = await fetch(pictureUri);
-    // const blob = await response.blob();
-    // const result = await fetch(uploadUrl, {
-    //   method: "POST",
-    //   headers: { "Content-Type": blob.type },
-    //   body: blob,
-    // });
-    // const { storageId } = await result.json();
   },
 });
 
