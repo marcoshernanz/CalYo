80daa78c9ae0deffaa4fd091ae3b3e4f85e5d673
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-16 15:46:33 +0200

    meals schema

--- convex/tables/meals.ts
+++ convex/tables/meals.ts
@@ -1,11 +1,27 @@
 import { defineTable } from "convex/server";
 import { v } from "convex/values";
 
-export const mealsFields = {
-  userId: v.id("users"),
-  photoStorageId: v.string(), // TODO: photo, image or picture?
-  name: v.string(),
-  // Nutrients? Or just compute from mealItems?
-};
+export const mealsFields = v.union(
+  v.object({
+    userId: v.id("users"),
+    status: v.literal("pending"),
+  }),
+  v.object({
+    userId: v.id("users"),
+    name: v.string(),
+    photoStorageId: v.string(), // TODO: photo, image or picture?
+    status: v.union(
+      v.literal("processing"),
+      v.literal("done"),
+      v.literal("error")
+    ),
+    totals: v.object({
+      calories: v.number(),
+      protein: v.number(),
+      fat: v.number(),
+      carbs: v.number(),
+    }),
+  })
+);
 
 export const meals = defineTable(mealsFields);
