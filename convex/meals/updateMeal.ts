88838d1e0da44f615f1325660fa878142ae61194
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-16 18:58:42 +0200

    make updateMeal optional

--- convex/meals/updateMeal.ts
+++ convex/meals/updateMeal.ts
@@ -1,10 +1,32 @@
 import { getAuthUserId } from "@convex-dev/auth/server";
 import { mutation } from "../_generated/server";
-import { mealsFields } from "../tables/meals";
 import { v } from "convex/values";
 
 const updateMeal = mutation({
-  args: { id: v.id("meals"), meal: v.object(mealsFields) },
+  args: {
+    id: v.id("meals"),
+    meal: v.object({
+      userId: v.optional(v.id("users")),
+      status: v.optional(
+        v.union(
+          v.literal("pending"),
+          v.literal("processing"),
+          v.literal("done"),
+          v.literal("error")
+        )
+      ),
+      name: v.optional(v.string()),
+      photoStorageId: v.optional(v.string()), // TODO: photo, image or picture?
+      totals: v.optional(
+        v.object({
+          calories: v.number(),
+          protein: v.number(),
+          fat: v.number(),
+          carbs: v.number(),
+        })
+      ),
+    }),
+  },
   handler: async (ctx, args) => {
     const userId = await getAuthUserId(ctx);
     if (userId === null) return; // TODO: Correct handling or throw error?
