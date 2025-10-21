9a393668bbd33bf895064e080c763b15dbd22f5c
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-21 14:26:50 +0200

    scale nutrients util

---
+++ lib/utils/scaleNutrients.ts
@@ -0,0 +1,17 @@
+interface Params {
+  nutrients: {
+    protein: number;
+    fat: number;
+    carbs: number;
+  };
+  grams: number;
+}
+
+export default function scaleNutrients({ nutrients, grams }: Params) {
+  const factor = grams / 100;
+  return {
+    protein: Math.round(nutrients.protein * factor),
+    fat: Math.round(nutrients.fat * factor),
+    carbs: Math.round(nutrients.carbs * factor),
+  };
+}
