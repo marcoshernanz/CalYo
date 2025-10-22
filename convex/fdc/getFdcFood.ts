1bc8f293a28584569ab2cce97d5af1252f1f6ff2
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-22 15:21:04 +0200

    add auth to getFdcFoods

--- convex/fdc/getFdcFood.ts
+++ convex/fdc/getFdcFood.ts
@@ -1,9 +1,13 @@
 import { v } from "convex/values";
 import { query } from "../_generated/server";
+import { getAuthUserId } from "@convex-dev/auth/server";
 
 const getFdcFood = query({
   args: { fdcId: v.number() },
   handler: async (ctx, { fdcId }) => {
+    const userId = await getAuthUserId(ctx);
+    if (userId === null) throw new Error("Unauthorized");
+
     const foodItem = await ctx.db
       .query("fdcFoods")
       .withIndex("byFdcId", (q) => q.eq("fdcId", fdcId))
