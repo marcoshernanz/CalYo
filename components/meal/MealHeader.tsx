d3150b952548a497316d179572e266445990d76b
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-24 10:09:06 +0200

    meal header

--- components/meal/MealHeader.tsx
+++ components/meal/MealHeader.tsx
@@ -1,3 +1,48 @@
+import { StyleSheet, View } from "react-native";
+import Button from "../ui/Button";
+import { useRouter } from "expo-router";
+import { ArrowLeftIcon, EllipsisVerticalIcon } from "lucide-react-native";
+import Text from "../ui/Text";
+
 export default function MealHeader() {
-  return null;
+  const router = useRouter();
+
+  return (
+    <View style={styles.container}>
+      <Button
+        size="sm"
+        variant="secondary"
+        style={styles.button}
+        onPress={() => router.back()}
+      >
+        <ArrowLeftIcon size={22} />
+      </Button>
+      <View style={styles.title}>
+        <Text size="18" weight="600">
+          Nutrición
+        </Text>
+      </View>
+      <Button size="sm" variant="secondary" style={styles.button}>
+        <EllipsisVerticalIcon size={22} />
+      </Button>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "100%",
+    paddingBottom: 16,
+  },
+  button: {
+    aspectRatio: 1,
+  },
+  title: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
