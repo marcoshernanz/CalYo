be3d1630128eb297ba1bcc08f28c2512abbc5a2e
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-24 10:00:26 +0200

    safe area

--- components/ui/SafeArea.tsx
+++ components/ui/SafeArea.tsx
@@ -1,23 +1,42 @@
 import getColor from "@/lib/ui/getColor";
 import { StyleSheet } from "react-native";
 import {
+  Edge,
+  Edges,
   SafeAreaView,
   SafeAreaViewProps,
   useSafeAreaInsets,
 } from "react-native-safe-area-context";
 
+const paddingVertical = 12;
+const paddingHorizontal = 16;
+
 export default function SafeArea({ style, ...props }: SafeAreaViewProps) {
-  return <SafeAreaView style={[styles.safeArea, style]} {...props} />;
+  const edges = props.edges;
+
+  return (
+    <SafeAreaView
+      style={[
+        styles.safeArea,
+        hasEdge(edges, "top") && { paddingTop: paddingVertical },
+        hasEdge(edges, "bottom") && { paddingBottom: paddingVertical },
+        hasEdge(edges, "left") && { paddingLeft: paddingHorizontal },
+        hasEdge(edges, "right") && { paddingRight: paddingHorizontal },
+        style,
+      ]}
+      {...props}
+    />
+  );
 }
 
 export function useSafeArea() {
   const insets = useSafeAreaInsets();
 
   return {
-    top: insets.top + styles.safeArea.paddingVertical,
-    bottom: insets.bottom + styles.safeArea.paddingVertical,
-    left: insets.left + styles.safeArea.paddingHorizontal,
-    right: insets.right + styles.safeArea.paddingHorizontal,
+    top: insets.top + paddingVertical,
+    bottom: insets.bottom + paddingVertical,
+    left: insets.left + paddingHorizontal,
+    right: insets.right + paddingHorizontal,
   };
 }
 
@@ -25,7 +44,17 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: getColor("background"),
-    paddingVertical: 12,
-    paddingHorizontal: 16,
   },
 });
+
+function hasEdge(edges: Edges | undefined, edge: Edge) {
+  if (edges === undefined) {
+    return true;
+  }
+
+  if (Array.isArray(edges)) {
+    return edges.includes(edge);
+  }
+
+  return false;
+}
