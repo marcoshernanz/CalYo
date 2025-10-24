2720f508ea77f9fb957136652b55e3c1a3f222e8
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-24 09:49:31 +0200

    useSafeArea

--- components/ui/SafeArea.tsx
+++ components/ui/SafeArea.tsx
@@ -3,10 +3,22 @@ import { StyleSheet } from "react-native";
 import {
   SafeAreaView,
   SafeAreaViewProps,
+  useSafeAreaInsets,
 } from "react-native-safe-area-context";
 
 export default function SafeArea({ style, ...props }: SafeAreaViewProps) {
   return <SafeAreaView style={[styles.safeArea, style]} {...props} />;
+}
+
+export function useSafeArea() {
+  const insets = useSafeAreaInsets();
+
+  return {
+    top: insets.top + styles.safeArea.paddingVertical,
+    bottom: insets.bottom + styles.safeArea.paddingVertical,
+    left: insets.left + styles.safeArea.paddingHorizontal,
+    right: insets.right + styles.safeArea.paddingHorizontal,
+  };
 }
 
 const styles = StyleSheet.create({
