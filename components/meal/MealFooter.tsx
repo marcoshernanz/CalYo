212ec1190af621fd5e15c209aaccea6a8c2ee80b
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-24 10:21:24 +0200

    meal footer

--- components/meal/MealFooter.tsx
+++ components/meal/MealFooter.tsx
@@ -1,3 +1,21 @@
+import { StyleSheet, View } from "react-native";
+import Button from "../ui/Button";
+
 export default function MealFooter() {
-  return null;
+  return (
+    <View style={styles.container}>
+      <Button style={styles.doneButton}>Hecho</Button>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    paddingTop: 12,
+  },
+  doneButton: {
+    flex: 1,
+    height: 48,
+  },
+});
