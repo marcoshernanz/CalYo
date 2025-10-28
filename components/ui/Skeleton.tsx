f6f3a71a584d2967cc804de4bfdac4398c0ddc5b
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-28 14:47:27 +0100

    skeleton

---
+++ components/ui/Skeleton.tsx
@@ -0,0 +1,39 @@
+import { StyleProp, StyleSheet, ViewStyle } from "react-native";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withRepeat,
+  withTiming,
+} from "react-native-reanimated";
+import { useEffect } from "react";
+import getColor from "@/lib/ui/getColor";
+
+interface Props {
+  style?: StyleProp<ViewStyle> | undefined;
+}
+
+export default function Skeleton({ style }: Props) {
+  const opacity = useSharedValue(0.6);
+
+  useEffect(() => {
+    opacity.value = withRepeat(
+      withTiming(1, { duration: 900, easing: Easing.inOut(Easing.ease) }),
+      -1,
+      true
+    );
+  }, [opacity]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    opacity: opacity.value,
+  }));
+
+  return <Animated.View style={[styles.base, animatedStyle, style]} />;
+}
+
+const styles = StyleSheet.create({
+  base: {
+    backgroundColor: getColor("muted"),
+    borderRadius: 8,
+  },
+});
