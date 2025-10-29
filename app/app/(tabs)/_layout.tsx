02b835aeb44cfaa8e61fd1d4e7773cd4057474af
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-29 09:13:38 +0100

    refactor tabs add button

--- app/app/(tabs)/_layout.tsx
+++ app/app/(tabs)/_layout.tsx
@@ -1,9 +1,10 @@
-import TabsAddButton from "@/components/tabs/TabsAddButton";
 import Button from "@/components/ui/Button";
 import getColor from "@/lib/ui/getColor";
-import { Tabs } from "expo-router";
-import { HomeIcon, SettingsIcon } from "lucide-react-native";
+import getShadow from "@/lib/ui/getShadow";
+import { Tabs, useRouter } from "expo-router";
+import { HomeIcon, PlusIcon, SettingsIcon } from "lucide-react-native";
 import { PressableProps, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 function TabBarButton(props: PressableProps) {
   return <Button {...props} />;
@@ -41,9 +42,10 @@ export default function TabLayout() {
           options={{
             tabBarLabel: "",
             tabBarIcon: () => null,
-            tabBarButton: () => null,
+            tabBarButton: () => <CenterAddButton />,
           }}
         />
+
         <Tabs.Screen
           name="settings"
           options={{
@@ -54,8 +56,29 @@ export default function TabLayout() {
           }}
         />
       </Tabs>
+    </View>
+  );
+}
 
-      <TabsAddButton />
+function CenterAddButton() {
+  const { bottom } = useSafeAreaInsets();
+  const router = useRouter();
+  const size = 59 + Math.max(0, bottom / 2 - 10);
+
+  return (
+    <View
+      pointerEvents="box-none"
+      style={{ flex: 1, alignItems: "center", top: -16 }}
+    >
+      <Button
+        variant="primary"
+        style={{ height: size, width: size, ...getShadow("sm") }}
+        hitSlop={10}
+        accessibilityLabel="Add"
+        onPress={() => router.push("/app/camera")}
+      >
+        <PlusIcon color={getColor("background")} size={28} />
+      </Button>
     </View>
   );
 }
