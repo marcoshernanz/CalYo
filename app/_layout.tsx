df210c5b0f255f63cbf91bd7dc9328e2d400c668
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-29 14:28:00 +0100

    react strict mode

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,16 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import RootLayoutProvider from "@/components/RootLayoutProvider";
 import { useAuthContext } from "@/context/AuthContext";
+import { StrictMode } from "react";
 
 export default function RootLayout() {
   return (
-    <RootLayoutProvider>
-      <StatusBar style="light" />
-      <RootNavigator />
-    </RootLayoutProvider>
+    <StrictMode>
+      <RootLayoutProvider>
+        <StatusBar style="light" />
+        <RootNavigator />
+      </RootLayoutProvider>
+    </StrictMode>
   );
 }
 
