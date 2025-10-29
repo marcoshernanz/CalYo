a2ed5960bf714d1103e9889bba296b6ee7534a93
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-29 15:20:57 +0100

    replace add button link with router.navigate

--- app/app/(tabs)/_layout.tsx
+++ app/app/(tabs)/_layout.tsx
@@ -1,7 +1,7 @@
 import Button from "@/components/ui/Button";
 import getColor from "@/lib/ui/getColor";
 import getShadow from "@/lib/ui/getShadow";
-import { Link, Tabs } from "expo-router";
+import { Tabs, useRouter } from "expo-router";
 import { HomeIcon, PlusIcon, SettingsIcon } from "lucide-react-native";
 import { PressableProps, View } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -61,6 +61,7 @@ export default function TabLayout() {
 }
 
 function CenterAddButton() {
+  const router = useRouter();
   const { bottom } = useSafeAreaInsets();
   const size = 59 + Math.max(0, bottom / 2 - 10);
 
@@ -69,16 +70,15 @@ function CenterAddButton() {
       pointerEvents="box-none"
       style={{ flex: 1, alignItems: "center", top: -16 }}
     >
-      <Link href="/app/camera" asChild>
-        <Button
-          variant="primary"
-          style={{ height: size, width: size, ...getShadow("sm") }}
-          hitSlop={10}
-          accessibilityLabel="Add"
-        >
-          <PlusIcon color={getColor("background")} size={28} />
-        </Button>
-      </Link>
+      <Button
+        variant="primary"
+        style={{ height: size, width: size, ...getShadow("sm") }}
+        hitSlop={10}
+        accessibilityLabel="Add"
+        onPress={() => router.navigate("/app/camera")}
+      >
+        <PlusIcon color={getColor("background")} size={28} />
+      </Button>
     </View>
   );
 }
