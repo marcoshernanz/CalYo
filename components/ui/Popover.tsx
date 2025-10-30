90af0965e14b793372e9c513ac9286f07162ce23
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-10-30 20:36:10 +0100

    popover animation

--- components/ui/Popover.tsx
+++ components/ui/Popover.tsx
@@ -1,16 +1,22 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, View } from "react-native";
 import Card from "./Card";
 import getShadow from "@/lib/ui/getShadow";
 import * as PopoverPrimitive from "@rn-primitives/popover";
 import Button from "./Button";
 import { useRef } from "react";
 import { TriggerRef } from "@rn-primitives/popover";
+import Animated, {
+  ZoomInEasyUp,
+  ZoomOutEasyDown,
+} from "react-native-reanimated";
 
 interface Props {
   trigger: React.ReactNode;
   options: { Item: React.ReactNode; onPress: () => void }[];
   width?: number;
 }
+
+const AnimatedCard = Animated.createAnimatedComponent(Card);
 
 export default function Popover({ trigger, options, width = 160 }: Props) {
   const popoverTriggerRef = useRef<TriggerRef>(null);
@@ -23,23 +29,28 @@ export default function Popover({ trigger, options, width = 160 }: Props) {
       <PopoverPrimitive.Portal>
         <PopoverPrimitive.Overlay style={StyleSheet.absoluteFill} />
         <PopoverPrimitive.Content asChild align="end" side="bottom">
-          <Card style={[styles.card]}>
-            {options.map((option, index) => (
-              <Button
-                variant="base"
-                size="sm"
-                key={`popover-option-${index}`}
-                onPress={() => {
-                  // option.onPress();
-                  // onClose();
-                  popoverTriggerRef.current?.close();
-                }}
-                style={styles.button}
-              >
-                {option.Item}
-              </Button>
-            ))}
-          </Card>
+          <View collapsable={false}>
+            <AnimatedCard
+              style={[styles.card, { minWidth: width }]}
+              entering={ZoomInEasyUp.duration(250)}
+              exiting={ZoomOutEasyDown.duration(250)}
+            >
+              {options.map((option, index) => (
+                <Button
+                  variant="base"
+                  size="sm"
+                  key={`popover-option-${index}`}
+                  onPress={() => {
+                    // option.onPress(); TODO
+                    popoverTriggerRef.current?.close();
+                  }}
+                  style={styles.button}
+                >
+                  {option.Item}
+                </Button>
+              ))}
+            </AnimatedCard>
+          </View>
         </PopoverPrimitive.Content>
       </PopoverPrimitive.Portal>
     </PopoverPrimitive.Root>
