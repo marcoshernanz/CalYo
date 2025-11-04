973b6baf43cec6f3357b377891669dd8c7156953
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-11-04 21:18:20 +0100

    fix meal scrollview grow

--- components/meal/Meal.tsx
+++ components/meal/Meal.tsx
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   scrollViewContent: {
-    flex: 1,
+    flexGrow: 1,
     paddingBottom: 16,
   },
   name: {
--- components/mealItem/MealItem.tsx
+++ components/mealItem/MealItem.tsx
@@ -73,7 +73,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   scrollViewContent: {
-    flex: 1,
+    flexGrow: 1,
     paddingBottom: 16,
   },
   name: {
