9432c23a40ef36c098c18d55dd1b7f7257d295ae
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-11-05 14:59:04 +0100

    getNeatMultiplier

---
+++ lib/nutrition/getNeatMultiplier.ts
@@ -0,0 +1,15 @@
+import { OnboardingValues } from "@/context/OnboardingContext";
+
+interface Params {
+  activityLevel: OnboardingValues["activityLevel"];
+}
+
+const neatMap: Record<OnboardingValues["activityLevel"], number> = {
+  low: 1.25,
+  medium: 1.45,
+  high: 1.65,
+};
+
+export default function getNeatMultiplier({ activityLevel }: Params) {
+  return neatMap[activityLevel];
+}
