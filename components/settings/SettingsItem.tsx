8afd17859ffd12de3fa4c4a87aac03224e03d6df
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-11-05 20:59:16 +0100

    settings item

---
+++ components/settings/SettingsItem.tsx
@@ -0,0 +1,43 @@
+import { StyleSheet, View } from "react-native";
+import Button from "../ui/Button";
+import getColor from "@/lib/ui/getColor";
+import { LucideIcon } from "lucide-react-native";
+import Text from "../ui/Text";
+
+type Props = {
+  text: string;
+  Icon: LucideIcon;
+  onPress: () => void;
+  isLast?: boolean;
+};
+
+export default function SettingsItem({ text, Icon, onPress, isLast }: Props) {
+  return (
+    <View style={[styles.container, !isLast && { borderBottomWidth: 1 }]}>
+      <Button
+        variant="base"
+        size="base"
+        style={styles.button}
+        onPress={onPress}
+      >
+        <Icon size={18} color={getColor("foreground")} />
+        <Text size="16" weight="500">
+          {text}
+        </Text>
+      </Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: getColor("muted"),
+  },
+  button: {
+    height: 52,
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+    paddingHorizontal: 16,
+  },
+});
