7903b5ab8b79aefd4cc2cbd091afa4d1e1a4c1f8
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-11-05 14:59:11 +0100

    estimateBodyFatPercentage

---
+++ lib/nutrition/estimateBodyFatPercentage.ts
@@ -0,0 +1,25 @@
+import { OnboardingValues } from "@/context/OnboardingContext";
+
+interface Params {
+  weight: number;
+  height: number;
+  age: number;
+  sex: OnboardingValues["sex"];
+}
+
+export default function estimateBodyFatPercentage({
+  weight,
+  height,
+  age,
+  sex,
+}: Params) {
+  const bmi = weight / (height * height);
+  const sexFlag = sex === "male" ? 1 : 0;
+  const rayBf = 1.2 * bmi + 0.23 * age - 10.8 * sexFlag - 5.4;
+
+  const maxBf = 55;
+  const minBf = 5;
+  const bodyFat = Math.max(minBf, Math.min(maxBf, rayBf));
+
+  return bodyFat;
+}
