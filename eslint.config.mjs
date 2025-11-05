ca31dcafe9c3205f7af578cca4d1b677fcf9588b
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-11-05 19:06:29 +0100

    eslint strict

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
 import eslint from "@eslint/js";
 import { defineConfig } from "eslint/config";
-import { configs as tseslintConfigs } from "typescript-eslint";
+import tseslint from "typescript-eslint";
 import expoConfig from "eslint-config-expo/flat.js";
 import reactCompiler from "eslint-plugin-react-compiler";
 
 export default defineConfig(
   eslint.configs.recommended,
-  tseslintConfigs.recommended,
   expoConfig,
-  reactCompiler.configs.recommended
+  reactCompiler.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic
 );
