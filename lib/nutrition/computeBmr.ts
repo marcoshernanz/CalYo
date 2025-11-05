80d0324c0f5184440a6434968d0120ba81607651
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-11-05 14:59:56 +0100

    computeBmr

---
+++ lib/nutrition/computeBmr.ts
@@ -0,0 +1,14 @@
+import { OnboardingValues } from "@/context/OnboardingContext";
+
+interface Params {
+  sex: OnboardingValues["sex"];
+  weight: number;
+  height: number;
+  age: number;
+}
+
+export default function computeBmr({ sex, weight, height, age }: Params) {
+  const add = sex === "male" ? 5 : -161;
+  const bmr = 10 * weight + 6.25 * height - 5 * age + add;
+  return bmr;
+}
