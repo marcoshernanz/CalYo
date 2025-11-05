b39680cc536f250a3e1c18736649a7254bf374ea
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-11-05 19:49:50 +0100

    fix linting errors

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,4 @@
-const { getDefaultConfig } = require("expo/metro-config");
+import { getDefaultConfig } from "expo/metro-config";
 
 module.exports = (() => {
   const config = getDefaultConfig(__dirname);
