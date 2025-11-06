be09efb4debd5e256ca310505edb1fef1a35afba
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-11-06 19:08:07 +0100

    screen footer

---
+++ components/ui/screen/ScreenFooter.tsx
@@ -0,0 +1,55 @@
+import { StyleSheet } from "react-native";
+import Button from "../Button";
+import SafeArea from "../SafeArea";
+import getColor from "@/lib/ui/getColor";
+import getShadow from "@/lib/ui/getShadow";
+import { ComponentProps } from "react";
+import Text from "../Text";
+import { LucideIcon, LucideProps } from "lucide-react-native";
+
+export function ScreenFooter({ children }: { children: React.ReactNode }) {
+  return (
+    <SafeArea edges={["bottom", "left", "right"]} style={styles.container}>
+      {children}
+    </SafeArea>
+  );
+}
+
+export function ScreenFooterButton(buttonProps: ComponentProps<typeof Button>) {
+  return <Button style={styles.button} {...buttonProps} />;
+}
+
+export function ScreenFooterButtonText({ text }: { text: string }) {
+  return (
+    <Text size="16" weight="600">
+      {text}
+    </Text>
+  );
+}
+
+export function ScreenFooterButtonIcon({
+  Icon,
+  ...props
+}: {
+  Icon: LucideIcon;
+} & LucideProps) {
+  return <Icon size={16} color={getColor("foreground")} {...props} />;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 0,
+    zIndex: 10,
+    flexDirection: "row",
+    paddingTop: 12,
+    position: "relative",
+    gap: 8,
+    ...getShadow("lg", { inverted: true }),
+  },
+  button: {
+    flex: 1,
+    height: 48,
+    flexDirection: "row",
+    gap: 4,
+  },
+});
