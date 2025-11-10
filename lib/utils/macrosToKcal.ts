c22a910719cf4e825baa38155e1c05e0a6b6b85b
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-11-10 13:42:22 +0100

    macros to kcal worklet

--- lib/utils/macrosToKcal.ts
+++ lib/utils/macrosToKcal.ts
@@ -2,12 +2,13 @@ type Params = {
   protein?: number;
   fat?: number;
   carbs?: number;
-}
+};
 
 export default function macrosToKcal({
   protein = 0,
   fat = 0,
   carbs = 0,
 }: Params): number {
+  "worklet";
   return protein * 4 + fat * 9 + carbs * 4;
 }
