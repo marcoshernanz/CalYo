7c452c7410b21e5b0660225785e417ddcf42aeb9
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-11-11 14:53:44 +0100

    profiles data

--- convex/tables/profiles.ts
+++ convex/tables/profiles.ts
@@ -9,6 +9,56 @@ export const profilesFields = {
     protein: v.number(),
     fat: v.number(),
   }),
+  data: v.optional(
+    v.object({
+      measurementSystem: v.union(v.literal("metric"), v.literal("imperial")),
+      sex: v.union(v.literal("male"), v.literal("female")),
+      bornDate: v.number(),
+      height: v.number(),
+      weight: v.number(),
+      weightTrend: v.union(
+        v.literal("lose"),
+        v.literal("maintain"),
+        v.literal("gain"),
+        v.literal("unsure")
+      ),
+      weeklyWorkouts: v.union(
+        v.literal("0-2"),
+        v.literal("3-5"),
+        v.literal("6+")
+      ),
+      activityLevel: v.union(
+        v.literal("low"),
+        v.literal("medium"),
+        v.literal("high")
+      ),
+      liftingExperience: v.union(
+        v.literal("none"),
+        v.literal("beginner"),
+        v.literal("intermediate"),
+        v.literal("advanced")
+      ),
+      cardioExperience: v.union(
+        v.literal("none"),
+        v.literal("beginner"),
+        v.literal("intermediate"),
+        v.literal("advanced")
+      ),
+      goal: v.union(
+        v.literal("lose"),
+        v.literal("maintain"),
+        v.literal("gain")
+      ),
+      targetWeight: v.number(),
+      weightChangeRate: v.number(),
+      training: v.union(
+        v.literal("none"),
+        v.literal("lifting"),
+        v.literal("cardio"),
+        v.literal("both")
+      ),
+    })
+  ),
 };
 
 export const profiles = defineTable(profilesFields).index("byUserId", [
