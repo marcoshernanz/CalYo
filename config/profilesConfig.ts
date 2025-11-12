d184cebc186e9dfe071f772a3fbecd66e8b625c2
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-11-12 16:00:07 +0100

    profiles config type safety

--- config/profilesConfig.ts
+++ config/profilesConfig.ts
@@ -1,10 +1,19 @@
+import { Doc } from "@/convex/_generated/dataModel";
 import macrosToKcal from "@/lib/utils/macrosToKcal";
+import { WithoutSystemFields } from "convex/server";
 
-export const profilesConfig = {
-  defaultTargets: {
-    calories: macrosToKcal({ carbs: 200, protein: 150, fat: 80 }),
-    carbs: 200,
-    protein: 150,
-    fat: 80,
+type ProfilesConfig = {
+  defaultValues: Omit<WithoutSystemFields<Doc<"profiles">>, "userId">;
+};
+
+export const profilesConfig: ProfilesConfig = {
+  defaultValues: {
+    targets: {
+      calories: macrosToKcal({ carbs: 200, protein: 150, fat: 80 }),
+      carbs: 200,
+      protein: 150,
+      fat: 80,
+    },
+    hasCompletedOnboarding: false,
   },
 } as const;
