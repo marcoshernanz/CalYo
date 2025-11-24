5139cc1703a3d9c5f177bace81666d78e9464f02
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-11-24 15:33:04 +0100

    remove externalId

--- convex/tables/foods.ts
+++ convex/tables/foods.ts
@@ -2,8 +2,6 @@ import { defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export const foodsFields = {
-  externalId: v.optional(v.string()),
-
   identity: v.union(
     v.object({
       source: v.literal("fdc"),
