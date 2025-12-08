016e0ea58906afdac8c19ef39d96ecee113b28b0
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-12-08 10:53:36 +0100

    bump up version

--- app.config.ts
+++ app.config.ts
@@ -22,7 +22,7 @@ const getAppName = () => {
 export default (): ExpoConfig => ({
   name: getAppName(),
   slug: "Calyo",
-  version: "1.0.4",
+  version: "1.0.5",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: "calyo",
