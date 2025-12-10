c8a493a3b8ffc4fc6f5f2adbbe6d8cc889a58d60
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-12-10 23:25:35 +0100

    meals total micros

--- convex/tables/meals.ts
+++ convex/tables/meals.ts
@@ -1,6 +1,6 @@
 import { defineTable } from "convex/server";
 import { v } from "convex/values";
-import { macrosFields, nutrientsFields } from "./mealItems";
+import { macrosFields, microsFields, nutrientsFields } from "./mealItems";
 
 export const mealsFields = {
   userId: v.id("users"),
@@ -14,6 +14,7 @@ export const mealsFields = {
   name: v.optional(v.string()),
   photoStorageId: v.optional(v.id("_storage")),
   totalMacros: v.optional(v.object(macrosFields)),
+  totalMicros: v.optional(v.object(microsFields)),
   totalNutrients: v.optional(v.object(nutrientsFields)),
 };
 
