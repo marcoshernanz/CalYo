e050b6c08ff430e1fa52cd9f60882291b2385353
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-12-15 23:52:54 +0100

    feat: add support for new food source with code field

--- convex/tables/foods.ts
+++ convex/tables/foods.ts
@@ -11,6 +11,10 @@ export const foodsFields = {
         v.literal("Legacy"),
         v.literal("Survey")
       ),
+    }),
+    v.object({
+      source: v.literal("off"),
+      code: v.string(),
     })
   ),
 
