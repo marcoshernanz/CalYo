0cc74ca8dd10cd08bb46a283b1cc03f2d6e3726e
Marcos Hernanz <marcos.hernanz.anton@gmail.com>
2025-12-20 13:56:01 +0100

    feat: add grams input field to MealItem for customizable serving size

--- components/mealItem/MealItem.tsx
+++ components/mealItem/MealItem.tsx
@@ -16,6 +16,9 @@ import SafeArea from "../ui/SafeArea";
 import Carousel from "../ui/Carousel";
 import MealMicros from "../meal/MealMicros";
 import scaleMicrosPer100g from "@/lib/utils/nutrition/scaleMicrosPer100g";
+import TextInput from "../ui/TextInput";
+import { useState } from "react";
+import { StyleSheet, View } from "react-native";
 
 type Props = {
   mealItemId: Id<"mealItems">;
@@ -31,6 +34,9 @@ export default function MealItem({
   loading,
 }: Props) {
   const { scrollY, onScroll } = useScrollY();
+  const [grams, setGrams] = useState<number | undefined>(
+    mealItem?.grams ?? 100
+  );
 
   const macrosPer100g = mealItem?.macrosPer100g ?? {
     calories: 0,
@@ -65,8 +71,25 @@ export default function MealItem({
         scrollViewProps={{ onScroll }}
         safeAreaProps={{ edges: [] }}
       >
-        <SafeArea edges={["left", "right"]} style={{ flex: 0 }}>
-          <ScreenMainTitle title={name} loading={loading} />
+        <SafeArea edges={["left", "right"]} style={styles.safeArea}>
+          <View style={styles.headerContainer}>
+            <ScreenMainTitle
+              title={name}
+              loading={loading}
+              style={styles.title}
+            />
+            <TextInput
+              value={`${String(grams ?? "")}g`}
+              onChangeText={(text) => {
+                const numberText = text.replace(/[^0-9]/g, "");
+                setGrams(numberText === "" ? undefined : Number(numberText));
+              }}
+              inputMode="numeric"
+              maxLength={8}
+              cardStyle={styles.textInputCard}
+              style={{ textAlign: "center" }}
+            />
+          </View>
         </SafeArea>
         <Carousel style={{ paddingBottom: 32 }}>
           <MealMacros macros={totalMacros} loading={loading} />
@@ -81,3 +104,24 @@ export default function MealItem({
     </ScreenMain>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 0,
+  },
+  headerContainer: {
+    flexDirection: "row",
+    gap: 12,
+    paddingBottom: 16,
+    alignItems: "center",
+  },
+  title: {
+    flex: 1,
+    paddingBottom: 0,
+  },
+  textInputCard: {
+    borderRadius: 999,
+    paddingVertical: 10,
+    minWidth: 100,
+  },
+});
